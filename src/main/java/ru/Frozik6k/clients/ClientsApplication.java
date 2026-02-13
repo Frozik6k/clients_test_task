b5441fb0e704398d4651fcc61d854ea4a82d5614
Frozik6k <frozik6k@yandex.ru>
2026-02-13 20:47:40 +0500

    commit initial

---
+++ src/main/java/ru/Frozik6k/clients/ClientsApplication.java
@@ -0,0 +1,13 @@
+package ru.Frozik6k.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Frozik6k/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Frozik6k.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
