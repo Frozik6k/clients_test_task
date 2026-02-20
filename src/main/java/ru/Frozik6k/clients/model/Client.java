127cf12435697d78b6d3c868786c0a96c704adff
Frozik6k <frozik6k@yandex.ru>
2026-02-20 17:08:16 +0500

    Добавил таблицы и сущности client и контакт

---
+++ src/main/java/ru/Frozik6k/clients/model/Client.java
@@ -0,0 +1,37 @@
+package ru.Frozik6k.clients.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Client {
+    @Id
+    @Column(name = "client_id", nullable = false, updatable = false)
+    private UUID clientId;
+
+    @Column(length = 64)
+    private String name;
+
+    @Column(name = "last_name", length = 64)
+    private String lastName;
+
+    @OneToMany(
+            mappedBy = "client",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Contact> contacts;
+
+    @PrePersist
+    void prePersist() {
+        if (clientId == null) clientId = UUID.randomUUID();
+    }
+}
